e85ad02c1df370fb5a0e0f9d99c462a021213201
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-20 23:25:03 +0200

    finished calculator logic

--- src/App.js
+++ src/App.js
@@ -62,6 +62,23 @@ function reducer(state, { type, payload }) {
       }
     case ACTIONS.CLEAR: 
       return {}
+    case ACTIONS.DELETE_DIGIT: 
+      if (state.overwrite) {
+        return {
+          ...state, 
+          overwrite: false, 
+          currentOperand: null 
+        }
+      }
+      if (state.currentOperand == null) return state 
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null }
+      }
+
+      return {
+        ...state, 
+        currentOperand: state.currentOperand.slice(0, -1)
+      }
     case ACTIONS.EVALUATE: 
       if (
         state.operation == null || 
@@ -104,6 +121,16 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0, 
+})
+function formatOperand(operand) {
+  if (operand == null) return 
+  const [integer, decimal] = operand.split('.')
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer, 
@@ -114,12 +141,12 @@ function App() {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-          {previousOperand} {operation}
+          {formatOperand(previousOperand)} {operation}
         </div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
